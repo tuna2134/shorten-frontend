4473a3b5337adb340a1b5056ca1715ffb6f24521
tuna2134 <masato.04.11.2007@gmail.com>
2022-12-07 10:09:09 +0000

    okay

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+      <>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="/">Shorten</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav.Link href="/about">About</Nav.Link>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </>
+    )
 }
