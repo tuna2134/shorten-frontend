bd07c9020970f296ba7327ecd7e20eecec2ad84f
tuna2134 <masato.04.11.2007@gmail.com>
2022-12-08 01:58:30 +0000

    fix: format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,35 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 // import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-      <>
-        <Head>
-          <title>Shorten</title>
-        </Head>
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <Navbar.Brand href="/">Shorten</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav.Link href="/about">About</Nav.Link>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <main>
-          <Component {...pageProps} />
-        </main>
-        <ToastContainer />
-      </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Shorten</title>
+      </Head>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Shorten</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav.Link href="/about">About</Nav.Link>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <ToastContainer />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,64 @@
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import { Container } from 'react-bootstrap'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import React from 'react'
-import axios from 'axios'
-import { toast } from 'react-toastify'
-import { TailSpin } from 'react-loader-spinner'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Container } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import React from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { TailSpin } from "react-loader-spinner";
 
 export default function Home() {
-    const [result, setResult] = React.useState(<Container></Container>)
-    function handleClick(url: string) {
-        navigator.clipboard.writeText(url)
-        toast.success("Copied to clipboard!")
-    }
-    async function handleSubmit(event: React.FormEvent) {
-        event.preventDefault()
-        if (!(event.target instanceof HTMLFormElement)) return
-        setResult(
-            <Container>
-                <TailSpin />
-            </Container>
-        )
-        const r = await axios.post("https://shor.f5.si/shorten", {
-            url: event.target.url.value,
-        })
-        setResult(
-          <Container>
-            <Row>
-                <Col xs={3}>
-                    <p>Result: {r.data}</p>
-                </Col>
-                <Col>
-                    <Button variant="primary" onClick={() => {handleClick(r.data)}}>Copy</Button>
-                </Col>
-            </Row>
-          </Container>
-        )
-        toast.success("URL shortened successfully!")
-    }
-    return (
-        <>
-          <h2 className="home-title">短縮しよう！</h2>
-          <Form onSubmit={handleSubmit} className="mb-3">
-            <Form.Group className="mb-3">
-                <Form.Label>URL</Form.Label>
-                <Form.Control type="text" placeholder="Enter URL" name="url" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Create
+  const [result, setResult] = React.useState(<Container></Container>);
+  function handleClick(url: string) {
+    navigator.clipboard.writeText(url);
+    toast.success("Copied to clipboard!");
+  }
+  async function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    if (!(event.target instanceof HTMLFormElement)) return;
+    setResult(
+      <Container>
+        <TailSpin />
+      </Container>
+    );
+    const r = await axios.post("https://shor.f5.si/shorten", {
+      url: event.target.url.value,
+    });
+    setResult(
+      <Container>
+        <Row>
+          <Col xs={3}>
+            <p>Result: {r.data}</p>
+          </Col>
+          <Col>
+            <Button
+              variant="primary"
+              onClick={() => {
+                handleClick(r.data);
+              }}
+            >
+              Copy
             </Button>
-          </Form>
-          {result}
-        </>
-    )
+          </Col>
+        </Row>
+      </Container>
+    );
+    toast.success("URL shortened successfully!");
+  }
+  return (
+    <>
+      <h2 className="home-title">短縮しよう！</h2>
+      <Form onSubmit={handleSubmit} className="mb-3">
+        <Form.Group className="mb-3">
+          <Form.Label>URL</Form.Label>
+          <Form.Control type="text" placeholder="Enter URL" name="url" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Create
+        </Button>
+      </Form>
+      {result}
+    </>
+  );
 }
