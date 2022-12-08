e4eed1bd424355d2df3ff9ae8cc930ebb008090a
tuna2134 <masato.04.11.2007@gmail.com>
2022-12-08 01:28:42 +0000

    add: title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,16 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
       <>
+        <Head>
+          <title>Shorten</title>
+        </Head>
         <Navbar bg="light" expand="lg">
           <Container>
             <Navbar.Brand href="/">Shorten</Navbar.Brand>
